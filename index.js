b03fff3c24ec416e3596da7918f8b86c53cd3517
Nick Meldrum <nick@nickmeldrum.com>
2017-03-14 18:09:14 +0000

    adds required cors headers

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ www.use(express.static('client'))
 www.get('/api/products/', (req, res) => { res.json('{"data": "returned from www"}') })
 www.listen(3081)
 
+api.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://www.website.com:3080')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  next()
+})
 api.get('/api/products/', (req, res) => { res.json('{"data": "returned from api"}') })
 api.listen(3082)
