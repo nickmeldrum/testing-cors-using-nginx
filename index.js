62c48c42c409d15ba9dfcc9474aa7c1e9b31b10c
Nick Meldrum <nick@nickmeldrum.com>
2017-03-14 18:27:10 +0000

    shows sending origin cookies over CORS

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const www = express()
 const api = express()
 
+www.use((req, res, next) => {
+  res.cookie('mycookie', 'some random data', { domain: '.website.com', maxAge: 900000, httpOnly: true })
+  next()
+})
 www.use(express.static('client'))
 www.get('/api/products/', (req, res) => { res.json('{"data": "returned from www"}') })
 www.listen(3081)
 
+api.use(cookieParser())
 api.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://www.website.com:3080')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
@@ -13,5 +19,8 @@ api.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true')
   next()
 })
-api.get('/api/products/', (req, res) => { res.json('{"data": "returned from api"}') })
+api.use('/api/products/', (req, res) => {
+  console.log(req.cookies.mycookie)
+  res.json('{"data": "returned from api"}')
+})
 api.listen(3082)
